d4b9c960529aa50c80c9fce33c67c848b04333b2
Asadulla56 <mdasadullahoque294@gmail.com>
2025-01-26 01:16:42 +0600

    Home route setuo

--- src/Routes/Mainroute.jsx
+++ src/Routes/Mainroute.jsx
@@ -4,11 +4,17 @@ import {
    
   } from "react-router-dom";
 import Root from "../Layouts/Root";
+import Home from "../pages/Home/Home";
 
 const Mainroute = createBrowserRouter([
     {
       path: "/",
       element: <Root/>,
+      children:[{
+        path: "/",
+        element:<Home></Home>
+      }]
     },
+
   ]);
   export default  Mainroute;
