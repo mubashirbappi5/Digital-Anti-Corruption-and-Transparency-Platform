38e47d5d392e04888b3b4212b9f4b8c5d21dddf7
Asadulla56 <mdasadullahoque294@gmail.com>
2025-02-09 00:14:39 +0600

    About pages information update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,10 +2,42 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='min-h-screen'>
-            <h1>This is About pages</h1>
-        </div>
-    );
+        <div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes"
+      className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div
+  className="hero min-h-screen"
+  style={{
+    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+  }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+</div>
+
+);
 };
 
 export default About;
