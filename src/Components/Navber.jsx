40c5182a94c93e4f77382098bfda37f96768d628
Asadulla56 <mdasadullahoque294@gmail.com>
2025-02-09 00:03:02 +0600

    About pages update

--- src/Components/Navber.jsx
+++ src/Components/Navber.jsx
@@ -11,7 +11,7 @@ const Navber = () => {
     <Link  className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700" >Resources</Link>
     <Link  to={'/support'} className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700" >Support</Link>
    
-    <Link  className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700" >About Us</Link>
+    <Link to={'/about'}  className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700" >About Us</Link>
 </>
     return (
         <div>
