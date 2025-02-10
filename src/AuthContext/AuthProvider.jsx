768770d2e18a2d60d7b2591723bf42c6dba18d6c
mubashirbappi <mubashirbappi@gmail.com>
2025-02-10 23:18:28 +0600

    google auth added

--- src/AuthContext/AuthProvider.jsx
+++ src/AuthContext/AuthProvider.jsx
@@ -1,9 +1,18 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React, { createContext } from 'react';
+import { auth } from '../Firebace/Firebace.init';
 export const Authcontext = createContext()
 const AuthProvider = ({children}) => {
 
+    const provider = new GoogleAuthProvider();
+
+    const googlesignin = ()=>{
+        return signInWithPopup(auth, provider)
+    }
+
     const authinfo = {
-        name:'bappi'
+        name:'bappi',
+        googlesignin,
 
     }
     return (
--- src/pages/Auth/SocialLogin/SocialLogin.jsx
+++ src/pages/Auth/SocialLogin/SocialLogin.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Authcontext } from '../../../AuthContext/AuthProvider';
 
 const SocialLogin = () => {
+    const {googlesignin}=useContext(Authcontext)
+   const handlegoogle = ()=>{
+    googlesignin()
+    .then(res=>{
+        console.log(res.user)
+    })
+    .catch(error=>{
+        console.log(error)
+    })
+   }
+
+
+
     return (
         <div className=' flex flex-col justify-center'>
              <div className="divider">OR</div>
-            <button className='btn btn-outline'>Google Login</button>
+            <button onClick={handlegoogle} className='btn btn-outline'>Google Login</button>
             
         </div>
     );
