e5ad13061685999e2a444214aea0e542ce7ebca6
Asadulla56 <mdasadullahoque294@gmail.com>
2025-02-12 01:23:53 +0600

    Feed nevbar added

--- src/Components/Navber.jsx
+++ src/Components/Navber.jsx
@@ -20,12 +20,16 @@ const Navber = () => {
       <Link className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">
         Home
       </Link>
-      <Link
+      {/* <Link
         to={"/dashboard"}
         className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
       >
         Dashboard
+      </Link> */}
+      <Link className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">
+        Feed
       </Link>
+      
       <Link
         to={"/projectTracking"}
         className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
