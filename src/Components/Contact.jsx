6d722e4ce89ed086b24dc674bfcbe268f2ff1814
Asadulla56 <mdasadullahoque294@gmail.com>
2025-02-12 11:57:59 +0600

    footer session update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,35 +1,66 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div className='bg-gray-200'>
-            <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
-	<div className="flex flex-col justify-between">
-		<div className="space-y-2">
-			<h2 className="text-4xl font-bold leading-tight lg:text-5xl">Get in Touch!</h2>
-			<div className="dark:text-gray-600">Have questions or need assistance? Reach out to us</div>
-		</div>
-		<img src="https://i.ibb.co.com/T26DL1X/Mobile-user-rafiki.png" alt="" className="p-6 w-52 md:w-64" />
-	</div>
-	<form noValidate="" className="space-y-6">
-		<div>
-			<label htmlFor="name" className="text-sm">Full name</label>
-			<input id="name" type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-100" />
-		</div>
-		<div>
-			<label htmlFor="email" className="text-sm">Email</label>
-			<input id="email" type="email" className="w-full p-3 rounded dark:bg-gray-100" />
-		</div>
-		<div>
-			<label htmlFor="message" className="text-sm">Message</label>
-			<textarea id="message" rows="3" className="w-full p-3 rounded dark:bg-gray-100"></textarea>
-		</div>
-		<button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-violet-600 text-gray-50">Send Message</button>
-	</form>
-</div>
-            
+  return (
+    <div className="bg-white">
+      <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
+        <div className="flex flex-col justify-between">
+          <div className="space-y-2">
+            <h2 className="text-4xl font-bold leading-tight lg:text-5xl">
+              Get in Touch!
+            </h2>
+            <div className="dark:text-gray-600">
+              Have questions or need assistance? Reach out to us
+            </div>
+          </div>
+          <img
+            src="https://i.ibb.co.com/T26DL1X/Mobile-user-rafiki.png"
+            alt=""
+            className="p-6 w-52 md:w-64"
+          />
         </div>
-    );
+        <form noValidate="" className="space-y-6">
+          <div>
+            <label htmlFor="name" className="text-sm">
+              Full name
+            </label>
+            <input
+              id="name"
+              type="text"
+              placeholder=""
+              className="w-full p-3 rounded dark:bg-gray-100"
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="text-sm">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="w-full p-3 rounded dark:bg-gray-100"
+            />
+          </div>
+          <div>
+            <label htmlFor="message" className="text-sm">
+              Message
+            </label>
+            <textarea
+              id="message"
+              rows="3"
+              className="w-full p-3 rounded dark:bg-gray-100"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-violet-600 text-gray-50"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
